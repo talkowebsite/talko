bf47db731cf1e36e5255b2f708a11f3a9b0a08e1
Genki Oji <genoji37@gmail.com>
2017-10-29 03:37:55 -0500

    fixes?

--- page_genki/get-place.js
+++ page_genki/get-place.js
@@ -54,9 +54,43 @@ recognition.onresult = function(event) {
   //var keyword = word + ",360degrees";
   var keyword = "antarctica,360degrees";
   console.log(keyword);
-  $(function(){
 
-        $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+  getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+        {
+            tags: keyword,
+            tagmode: "any",
+            format: "json"
+        },
+        function(data) {
+            var rnd = Math.floor(Math.random() * data.items.length);
+
+            var image_src = data.items[rnd]['media']['m'].replace("_m", "_b");
+            pic.src = image_src;
+            console.log(image_src);
+        }
+
+  // google.load('search', '1');
+  // google.setOnLoadCallback(OnLoad);
+  // var search;
+
+  // function OnLoad()
+  //   {
+  //       search = new google.search.ImageSearch();
+  //       search.setSearchCompleteCallback(this, searchComplete, null);
+  //       search.execute(keyword);
+  //   }
+  //   function searchComplete()
+  //   {
+  //       if (search.results && search.results.length > 0)
+  //       {
+  //           var rnd = Math.floor(Math.random() * search.results.length);
+  //           console.log(search.results[rnd]['url']);
+  //       }
+  //   }
+/*
+  //$(function(){
+
+        getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
         {
             tags: keyword,
             tagmode: "any",
@@ -70,9 +104,10 @@ recognition.onresult = function(event) {
             console.log(image_src);
         });
 
-    });
+    }
 
 }
+*/
 recognition.onspeechend = function() {
   recognition.stop();
   console.log('ended sound recognition');
