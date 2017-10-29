085423d0597d2232ff4be09ff49c772d2393bed9
Genki Oji <genoji37@gmail.com>
2017-10-29 09:25:41 -0500

    trying things desperately...

--- page_genki/components/set-image.js
+++ page_genki/components/set-image.js
@@ -4,7 +4,6 @@
  * Component that listens to an event, fades out an entity, swaps the texture, and fades it
  * back in.
  */
-
 AFRAME.registerComponent('set-image', {
   schema: {
     on: {type: 'string'},
@@ -14,7 +13,6 @@ AFRAME.registerComponent('set-image', {
   },
 
   init: function () {
-    this.getImageURL();
     var data = this.data;
     var el = this.el;
 
@@ -31,39 +29,7 @@ AFRAME.registerComponent('set-image', {
     });
   },
 
-  getImageURL: function(){
-    var keyword = document.getElementById('myVar').value;
-    var params = {
-            // Request parameters
-            "q": keyword
-        };
-        $.ajaxSetup({async:false});
-        $.ajax({
-            url: "https://api.cognitive.microsoft.com/bing/v7.0/images/search?" + $.param(params),
-            beforeSend: function(xhrObj){
-                // Request headers
-                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","feed4b52267b4552953ae546dd729bd4");
-            },
-            type: "GET",
-            // Request body
-            data: "",
-        })
-        .done(function(data) {
-            //alert("success");
-            console.log('success');
-            var rnd = Math.floor(Math.random() * (data.length/3)); 
-
-            //var image_src = data.d.results[0].Image[rnd].MediaUrl;
-            var result = JSON.stringify(data.value[0].contentUrl);
-            console.log(result);
-            var element = document.getElementById('city');
-            elementsrc = result;
-        })
-        .fail(function() {
-            //alert("error");
-        });
-      },
-
+  
   /**
    * Setup fade-in + fade-out.
    */
